3d8e7650dc900432847b9a96f5c7871704786051
daniel florez <daniel0223@hotmail.es>
2023-09-08 20:44:30 -0500

    ajuste src/main/resources/application.properties

--- src/main/java/com/app/ejemplo/controller/UserController.java
+++ src/main/java/com/app/ejemplo/controller/UserController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/user")
